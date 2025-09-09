3b1810100b1bd156c9512a0c075950dc48dcf150
Kirill Suhanov <kirillikirill86@gmail.com>
2025-09-09 20:48:39 +0300

    initialization of variables for utilities operation

--- Lab_1/creating_processes/creating_processes.cpp
+++ Lab_1/creating_processes/creating_processes.cpp
@@ -1,7 +1,26 @@
 ﻿#include <iostream>
+#include <string> 
+
+using namespace std;
+
+struct employee
+{
+    int num; // идентификационный номер сотрудника
+    char name[10]; // имя сотрудника
+    double hours; // количество отработанных часов
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string binfName, tmp;
+    int numRec;
+
+    cout << "Введите имя бинарного файла и количество записей в этом файле";
+    cin >> binfName;
+    cin >> numRec;
+
+    tmp = "Creator.exe " + binfName + " " + to_string(numRec);
+
+    cout << "Была вызвана утилита Creator";
 }
 
