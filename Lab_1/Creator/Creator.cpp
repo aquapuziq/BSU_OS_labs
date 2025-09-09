f78bc2b83faa0b50d36e67f9f8a25cd8c4312106
Kirill Suhanov <kirillikirill86@gmail.com>
2025-09-09 21:12:19 +0300

    creation of the utility "Creator". Entering the list of employees

---
+++ Lab_1/Creator/Creator.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct employee
+{
+    int num; // идентификационный номер сотрудника
+    char name[10]; // имя сотрудника
+    double hours; 
+};
+
+int main(int argc, char* argv[])
+{
+    int numRec = atoi(argv[2]);
+    employee* employees = new employee[numRec];
+
+    cout << "Введите список сотрудников:" << endl;
+    for (int i = 1; i <= numRec; i++) {
+        cout << i << ".";
+        cin >> employees[i].num;
+        cin >> employees[i].name;
+        cin >> employees[i].hours;
+    }
+
+}
