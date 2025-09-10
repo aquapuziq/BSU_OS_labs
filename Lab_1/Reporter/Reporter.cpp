baba1bde4721137310f74636ccdccdeca757c3b0
Kirill Suhanov <kirillikirill86@gmail.com>
2025-09-10 18:11:57 +0300

    full Reporter

--- Lab_1/Reporter/Reporter.cpp
+++ Lab_1/Reporter/Reporter.cpp
@@ -19,5 +19,21 @@ int wmain(int argc, wchar_t* argv[])
     ofstream rept(argv[2]);
     float payPerHour = _wtoi(argv[3]);
 
+    vector<employee> employees;
+    employee tmpEmployee;
+
+
+    while (binf.read((char*)&tmpEmployee, sizeof(employee))) {
+        employees.push_back(tmpEmployee);
+    }
+
+    rept << "Отчёт по файлу " << argv[1] << ":" << endl;
+    rept << "Номер сотрудника, имя сотрудника, часы, зарплата." << endl;
+
+    for (int i = 0; i < employees.size(); i++) {
+        rept << employees[i].num << " " << employees[i].name << " " <<
+            employees[i].hours << " " << employees[i].hours * payPerHour << endl;
+    }
+
     return  0;
 }
