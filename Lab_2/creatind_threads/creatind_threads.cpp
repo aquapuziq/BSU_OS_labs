2ec0a7017113a3da46f11cfabcbe853936244e9f
Kirill Suhanov <kirillikirill86@gmail.com>
2025-09-23 23:03:09 +0300

    init commit. Model for the main thread

---
+++ Lab_2/creatind_threads/creatind_threads.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+DWORD WINAPI min_max(LPVOID element) {}
+
+DWORD WINAPI average(LPVOID element) {}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int n; int a;
+    cout << "Введите размерность массива: ";
+    cin >> n;
+    int* arr = new int[n];
+
+    cout << "Введите элементы массива: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    HANDLE hMinMax, hAverage;
+    DWORD IDMinMax, IDAverage;
+
+
+    hMinMax = CreateThread(NULL, 0, min_max, (void*)a, 0, &IDMinMax);
+    if (hMinMax == NULL)
+        return GetLastError();
+    WaitForSingleObject(hMinMax, INFINITE);
+    CloseHandle(hMinMax);
+
+    hAverage = CreateThread(NULL, 0, average, (void*)a, 0, &IDAverage);
+    if (hAverage == NULL)
+        return GetLastError();
+
+    WaitForSingleObject(hAverage, INFINITE);
+    CloseHandle(hAverage);
+
+    WaitForSingleObject(hMinMax, INFINITE);
+    CloseHandle(hMinMax);
+
+    /*cout << "Массив после форматирования: ";
+    for (int i = 0; i < n; i++) {
+        if (a[i] == max || a[i] == min) {
+            a[i] = average;
+        }
+        cout << arr[i] << " ";
+    }*/
+}
